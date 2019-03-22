a29c54c54d37186c13097d1163026e39a361cd55
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-22 11:21:50 +0000

    Amended Birthday to refactor methods for single responsibility

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -2,10 +2,11 @@ class Birthday
   def self.days_until_birthday(birthday)
     current_date = DateTime.now
     birth_date = Date.parse(birthday)
-    if birth_date.mjd - current_date.mjd < 0
-      return 365 + (birth_date.mjd - current_date.mjd)
-    else
-      return birth_date.mjd - current_date.mjd
-    end
+    self.compare_dates(current_date, birth_date)
+  end
+
+  def self.compare_dates(current_date, birth_date)
+    return birth_date.mjd - current_date.mjd if birth_date.mjd - current_date.mjd >= 0
+    return 365 + (birth_date.mjd - current_date.mjd)
   end
 end
