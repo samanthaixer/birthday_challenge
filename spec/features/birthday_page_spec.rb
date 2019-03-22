641bfbe96433b2aa243215c267c7e58a296cd0f2
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-22 10:19:52 +0000

    Changed Go button and added drop-down list

--- spec/features/birthday_page_spec.rb
+++ spec/features/birthday_page_spec.rb
@@ -4,9 +4,9 @@ feature "Accepting name and birthday" do
 
     fill_in "name", with: "Sam"
     fill_in "day", with: "19"
-    fill_in "month", with: "March"
+    select "March", from: "month"
 
-    click_button "Go!"
+    click_button ""
     expect(page).to have_text("Sam")
   end
 end
@@ -17,9 +17,9 @@ feature "Receiving a Happy Birthday message" do
 
     fill_in "name", with: "Sam"
     fill_in "day", with: DateTime.now.strftime("%d")
-    fill_in "month", with: DateTime.now.strftime("%m")
+    select "#{DateTime.now.strftime("%B")}", from: "month"
 
-    click_button "Go!"
+    click_button ""
     expect(page).to have_text("Happy Birthday Sam!")
   end
 end
@@ -29,10 +29,10 @@ feature "Days until my birthday message" do
     visit '/'
 
     fill_in "name", with: "Sam"
-    fill_in "day", with: DateTime.now.strftime("%d").to_i - 5
-    fill_in "month", with: DateTime.now.strftime("%m")
+    fill_in "day", with: DateTime.now.strftime("%d").to_i - 10
+    select "#{DateTime.now.strftime("%B")}", from: "month"
 
-    click_button "Go!"
+    click_button ""
     expect(page).to have_text("Hello Sam, it is 355 days until your birthday")
   end
 end
